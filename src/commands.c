515ff1d46803fe153901302d8e278b47490db45f
LunaStev <youngjae681@gmail.com>
2024-10-07 22:46:54 +0900

    Delete unnecessary headers

--- src/commands.c
+++ src/commands.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <dirent.h>
 #include <unistd.h>
-#include <string.h>
-#include <errno.h>
 
 #define MAX_COMMAND_LENGTH 256
 #define MAX_PATH_LEN 4096
--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
 #include "commands.h"
 
